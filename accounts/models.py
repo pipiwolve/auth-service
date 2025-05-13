3bdd888d076a3d71d8b43f6494636b2bc7da19f5
pipiwoleve <cpj030415@gmail.com>
2025-05-13 17:14:38 +0200

    Fixed Docker compose version warning and applied migrations

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(unique=True)
     # 其他自定义字段
 
 # settings.py 中指定自定义用户模型
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+import re
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -8,6 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('username', 'email', 'password', 'phone')
 
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        if not re.search(r'[A-Za-z]', value) or not re.search(r'[0-9]', value):
+            raise serializers.ValidationError("Password must contain both letters and numbers.")
+        return value
+    
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         return user
