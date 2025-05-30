7fb3191209d05ba613166693b8dbb7a4cc5ff4da
pipiwoleve <cpj030415@gmail.com>
2025-05-30 09:06:11 +0200

    Update GET api request

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def ping(request):
+    return Response({"message": "Auth service is running."})
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def validate_permission(request):
